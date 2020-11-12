6cb74ac92ccf03979a654ecd875099b13a83eeb4
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-12 12:41:09 -0300

    feat: GeolocatorGPSServiceAdapter

---
+++ lib/features/addressing/data/GeolocatorGPSServiceAdapter.dart
@@ -0,0 +1,56 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:grocery_brasil_app/core/errors/exceptions.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/Location.dart';
+import 'GPSServiceAdapter.dart';
+
+const geoserviceTimeout = Duration(seconds: 5);
+
+class GeolocatorGPSServiceAdapter implements GPSServiceAdapter {
+  final GeolocatorPlatform geolocatorPlatform;
+
+  GeolocatorGPSServiceAdapter({@required this.geolocatorPlatform});
+
+  @override
+  Future<Location> getCurrentLocation() async {
+    try {
+      final locationSeriveEnabled =
+          await geolocatorPlatform.isLocationServiceEnabled();
+      if (!locationSeriveEnabled) {
+        throw AddressingException(
+            messageId: MessageIds.UNEXPECTED, message: 'GPS not enabled');
+      }
+      final geoserivcePermission = await geolocatorPlatform.checkPermission();
+      switch (geoserivcePermission) {
+        case LocationPermission.denied:
+        case LocationPermission.deniedForever:
+          await _requestGeoservicePermission();
+          break;
+        default:
+      }
+      final position = await geolocatorPlatform.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high, timeLimit: geoserviceTimeout);
+      return Location(lat: position.latitude, lon: position.longitude);
+    } catch (e) {
+      if (e is AddressingException) {
+        throw e;
+      }
+      throw AddressingException(
+          messageId: MessageIds.UNEXPECTED, message: e.toString());
+    }
+  }
+
+  Future<void> _requestGeoservicePermission() async {
+    final geoserivcePermission = await geolocatorPlatform.requestPermission();
+
+    switch (geoserivcePermission) {
+      case LocationPermission.denied:
+      case LocationPermission.deniedForever:
+        throw AddressingException(
+            messageId: MessageIds.UNEXPECTED, message: 'Not permitted');
+        break;
+      default:
+    }
+  }
+}
---
+++ test/features/addressing/data/GeolocatorGPSServiceAdapter_test.dart
@@ -0,0 +1,131 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:grocery_brasil_app/core/errors/exceptions.dart';
+import 'package:grocery_brasil_app/domain/Location.dart';
+import 'package:grocery_brasil_app/features/addressing/data/GPSServiceAdapter.dart';
+import 'package:grocery_brasil_app/features/addressing/data/GeolocatorGPSServiceAdapter.dart';
+import 'package:mockito/mockito.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MockGeolocatorPlatform extends Mock implements GeolocatorPlatform {}
+
+main() {
+  MockGeolocatorPlatform mockGeolocatorPlatform;
+  GPSServiceAdapter sut;
+  setUp(() {
+    mockGeolocatorPlatform = MockGeolocatorPlatform();
+    sut =
+        GeolocatorGPSServiceAdapter(geolocatorPlatform: mockGeolocatorPlatform);
+  });
+  group('GeolocatorGPSServiceAdapter', () {
+    test('should return the Location', () async {
+      //setup
+      final expected = Location(lat: 10.0, lon: 11.0);
+      when(mockGeolocatorPlatform.isLocationServiceEnabled())
+          .thenAnswer((realInvocation) async => true);
+
+      when(mockGeolocatorPlatform.checkPermission())
+          .thenAnswer((realInvocation) async => LocationPermission.whileInUse);
+
+      when(mockGeolocatorPlatform.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.high,
+              timeLimit: Duration(seconds: 5)))
+          .thenAnswer((realInvocation) async =>
+              Position(latitude: 10.0, longitude: 11.0));
+
+      //act
+      Location actual = await sut.getCurrentLocation();
+      //assert
+      expect(expected, equals(actual));
+      verify(mockGeolocatorPlatform.isLocationServiceEnabled());
+      verify(mockGeolocatorPlatform.checkPermission());
+      verify(mockGeolocatorPlatform.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+          timeLimit: Duration(seconds: 5)));
+
+      verifyNoMoreInteractions(mockGeolocatorPlatform);
+    });
+
+    test('should request permission once when not granted', () async {
+      //setup
+      final expected = Location(lat: 10.0, lon: 11.0);
+      when(mockGeolocatorPlatform.isLocationServiceEnabled())
+          .thenAnswer((realInvocation) async => true);
+
+      when(mockGeolocatorPlatform.checkPermission())
+          .thenAnswer((realInvocation) async => LocationPermission.denied);
+      when(mockGeolocatorPlatform.requestPermission())
+          .thenAnswer((realInvocation) async => LocationPermission.whileInUse);
+
+      when(mockGeolocatorPlatform.getCurrentPosition(
+              desiredAccuracy: LocationAccuracy.high,
+              timeLimit: Duration(seconds: 5)))
+          .thenAnswer((realInvocation) async =>
+              Position(latitude: 10.0, longitude: 11.0));
+
+      //act
+      Location actual = await sut.getCurrentLocation();
+      //assert
+      expect(expected, equals(actual));
+      verify(mockGeolocatorPlatform.isLocationServiceEnabled());
+      verify(mockGeolocatorPlatform.checkPermission());
+      verify(mockGeolocatorPlatform.requestPermission());
+      verify(mockGeolocatorPlatform.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+          timeLimit: Duration(seconds: 5)));
+
+      verifyNoMoreInteractions(mockGeolocatorPlatform);
+    });
+
+    test('should throw error when permission not granted', () async {
+      //setup
+      final expected = AddressingException(
+          messageId: MessageIds.UNEXPECTED, message: 'Not permitted');
+      when(mockGeolocatorPlatform.isLocationServiceEnabled())
+          .thenAnswer((realInvocation) async => true);
+
+      when(mockGeolocatorPlatform.checkPermission())
+          .thenAnswer((realInvocation) async => LocationPermission.denied);
+      when(mockGeolocatorPlatform.requestPermission())
+          .thenAnswer((realInvocation) async => LocationPermission.denied);
+
+      //act
+      //assert
+      expect(() async => await sut.getCurrentLocation(), throwsA(expected));
+      await untilCalled(mockGeolocatorPlatform.isLocationServiceEnabled());
+      await untilCalled(mockGeolocatorPlatform.checkPermission());
+      await untilCalled(mockGeolocatorPlatform.requestPermission());
+
+      verify(mockGeolocatorPlatform.isLocationServiceEnabled());
+      verify(mockGeolocatorPlatform.checkPermission());
+      verify(mockGeolocatorPlatform.requestPermission());
+
+      verifyNoMoreInteractions(mockGeolocatorPlatform);
+    });
+    test('should throw error  when LocationService is disabled', () {
+      //setup
+      final expected = AddressingException(
+          messageId: MessageIds.UNEXPECTED, message: 'GPS not enabled');
+      when(mockGeolocatorPlatform.isLocationServiceEnabled())
+          .thenAnswer((realInvocation) async => false);
+
+      //act
+      //assert
+      expect(() async => sut.getCurrentLocation(), throwsA(expected));
+      verify(mockGeolocatorPlatform.isLocationServiceEnabled());
+      verifyNoMoreInteractions(mockGeolocatorPlatform);
+    });
+    test('should throw AddressingException when some error occurs', () {
+      //setup
+      final expected = AddressingException(
+          messageId: MessageIds.UNEXPECTED, message: 'Exception: some error');
+      when(mockGeolocatorPlatform.isLocationServiceEnabled())
+          .thenThrow(Exception('some error'));
+      //act
+      //assert
+      expect(() async => await sut.getCurrentLocation(), throwsA(expected));
+      verify(mockGeolocatorPlatform.isLocationServiceEnabled());
+
+      verifyNoMoreInteractions(mockGeolocatorPlatform);
+    });
+  });
+}
