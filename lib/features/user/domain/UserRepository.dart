0aa5d11290e5152b1660f445b92f7b3a66177d5c
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-24 14:53:13 -0300

    fix: UserRepository extends UserRepositoryUpdate

--- lib/features/user/domain/UserRepository.dart
+++ lib/features/user/domain/UserRepository.dart
@@ -3,7 +3,7 @@ import 'package:dartz/dartz.dart';
 import '../../../core/errors/failures.dart';
 import '../../../domain/User.dart';
 
-abstract class UserRepository {
+abstract class UserRepository extends UserRepositoryUpdate {
   Future<Either<UserFailure, User>> getUserByUserId(String userId);
   Future<Either<UserFailure, User>> createUser(User user);
 }
