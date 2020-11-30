0ac7b68346b1cd0bdeae27b6d58538273a818867
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-11-30 16:10:39 -0300

    fix: unused import

--- test/features/secrets/data/SecretsServiceImpl_test.dart
+++ test/features/secrets/data/SecretsServiceImpl_test.dart
@@ -1,4 +1,3 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grocery_brasil_app/core/errors/exceptions.dart';
 import 'package:grocery_brasil_app/features/secrets/data/SecretDataSource.dart';
