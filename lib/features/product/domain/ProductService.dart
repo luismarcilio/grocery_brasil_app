6be52f6dc8292d9660e6d170026749c47a60239a
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-01 11:38:47 -0300

    refactor: listProductsByText

--- lib/features/product/domain/ProductService.dart
+++ lib/features/product/domain/ProductService.dart
@@ -2,11 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 
 import '../../../core/errors/failures.dart';
-import '../../../domain/Location.dart';
-import 'ProductPrices.dart';
+import 'ProductSearchModel.dart';
 
 abstract class ProductService {
-  Future<Either<ProductFailure, Stream<ProductPrices>>>
-      listProductsPricesByTextAndLocationUseCase(
-          {@required Location location, @required String text});
+  Future<Either<ProductFailure, Stream<ProductSearchModel>>> listProductsByText(
+      {@required String text});
 }
