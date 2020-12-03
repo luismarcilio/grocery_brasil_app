6943dc9b9d7a77cbdf25fd12ca0bc1e94d4304ff
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-03 13:29:07 -0300

    feat: add timeout configuration

---
+++ lib/core/config.dart
@@ -0,0 +1,4 @@
+const googleApiTimeout = Duration(seconds: 10);
+const serverlessFunction = Duration(seconds: 10);
+const gpsTimeout = Duration(seconds: 10);
+const elasticSearchTimeout = Duration(seconds: 10);
