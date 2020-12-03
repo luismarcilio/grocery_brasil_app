36d0c665f8475aa61040b5559f44df34202a5576
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-03 17:22:38 -0300

    feat: add geocode calculations

--- lib/features/addressing/data/GPSServiceAdapter.dart
+++ lib/features/addressing/data/GPSServiceAdapter.dart
@@ -1,5 +1,9 @@
-import 'package:grocery_brasil_app/domain/Location.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/Location.dart';
 
 abstract class GPSServiceAdapter {
   Future<Location> getCurrentLocation();
+  double distanceBetween({@required Location start, @required Location end});
+  List<Location> calculateTopLeftBottomRight({Location center, int distance});
 }
--- lib/features/addressing/data/GeolocatorGPSServiceAdapter.dart
+++ lib/features/addressing/data/GeolocatorGPSServiceAdapter.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:geolocator/geolocator.dart';
-import 'package:grocery_brasil_app/core/config.dart';
-import 'package:grocery_brasil_app/core/errors/exceptions.dart';
 import 'package:meta/meta.dart';
 
+import '../../../core/config.dart';
+import '../../../core/errors/exceptions.dart';
 import '../../../domain/Location.dart';
 import 'GPSServiceAdapter.dart';
 
@@ -51,5 +53,44 @@ class GeolocatorGPSServiceAdapter implements GPSServiceAdapter {
         break;
       default:
     }
+  }
+
+  @override
+  double distanceBetween({Location start, Location end}) {
+    return geolocatorPlatform.distanceBetween(
+        start.lat, start.lon, end.lat, end.lon);
+  }
+
+  @override
+  List<Location> calculateTopLeftBottomRight({Location center, int distance}) {
+    //bearing (clockwise from north)
+    final double topLeftBrng = -45.0;
+    final double bottomRightBrng = 135.0;
+    final topLeft = _calculateCoordinates(
+        center: center, distance: distance, bearing: topLeftBrng);
+    final bottomRight = _calculateCoordinates(
+        center: center, distance: distance, bearing: bottomRightBrng);
+    return [topLeft, bottomRight];
+  }
+
+  Location _calculateCoordinates(
+      {Location center, int distance, double bearing}) {
+    //source: https://www.movable-type.co.uk/scripts/latlong.html
+    // Formula:	φ2 = asin( sin φ1 ⋅ cos δ + cos φ1 ⋅ sin δ ⋅ cos θ )
+    // λ2 = λ1 + atan2( sin θ ⋅ sin δ ⋅ cos φ1, cos δ − sin φ1 ⋅ sin φ2 )
+    // where	φ is latitude, λ is longitude, θ is the bearing (clockwise from north), δ is the angular distance d/R;
+    // d being the distance travelled, R the earth’s radius
+
+    final double R = 6371.0e3;
+    final double phi1 = center.lat / 180 * pi;
+    final double lambda1 = center.lon / 180 * pi;
+    final double tetha = bearing / 180 * pi;
+    final double delta = distance / R;
+    final double phi2 =
+        asin(sin(phi1) * cos(delta) + cos(phi1) * sin(delta) * cos(tetha));
+    final double lambda2 = lambda1 +
+        atan2(sin(tetha) * sin(delta) * cos(phi1),
+            cos(delta) - sin(phi1) * sin(phi2));
+    return Location(lat: 180 * phi2 / pi, lon: 180 * lambda2 / pi);
   }
 }
