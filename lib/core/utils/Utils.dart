42c582516154092dfe7afe67ea347ad1b93a543f
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-04 16:51:56 -0300

    refactor: dateTimeFromJsonOrTimestamp

--- lib/core/utils/Utils.dart
+++ lib/core/utils/Utils.dart
@@ -1,4 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 String enumToString(Object o) => o.toString().split('.').last;
 
 T enumFromString<T>(String key, List<T> values) =>
     values.firstWhere((v) => key == enumToString(v), orElse: () => null);
+
+DateTime dateTimeFromJsonOrTimestamp(dynamic input) {
+  if (input == null) {
+    return null;
+  }
+  if (input is String) {
+    return DateTime.parse(input);
+  }
+  if (input is Timestamp) {
+    return DateTime.fromMillisecondsSinceEpoch(input.millisecondsSinceEpoch);
+  }
+  throw Exception(
+      '$input: ${input.runtimeType.toString()} cannot be converted to DateTime');
+}
--- lib/domain/FiscalNote.dart
+++ lib/domain/FiscalNote.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+import '../core/utils/Utils.dart';
 import 'Company.dart';
 
 part 'FiscalNote.g.dart';
@@ -13,7 +13,7 @@ class FiscalNote extends Equatable {
   final String accessKey;
   final String number;
   final String series;
-  @JsonKey(fromJson: _dateTimeFromJsonOrTimestamp)
+  @JsonKey(fromJson: dateTimeFromJsonOrTimestamp)
   final DateTime date;
   final Company company;
 
@@ -28,15 +28,4 @@ class FiscalNote extends Equatable {
 
   @override
   List<Object> get props => [accessKey, number, series, date, company];
-
-  static DateTime _dateTimeFromJsonOrTimestamp(dynamic input) {
-    if (input is String) {
-      return DateTime.parse(input);
-    }
-    if (input is Timestamp) {
-      return DateTime.fromMillisecondsSinceEpoch(input.millisecondsSinceEpoch);
-    }
-    throw Exception(
-        '$input: ${input.runtimeType.toString()} cannot be converted to DateTime');
-  }
 }
