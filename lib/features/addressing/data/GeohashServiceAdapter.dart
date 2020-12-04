9e2c22faf4b9e8afaf6b00f6dcc30590ebf1d72a
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-04 12:02:39 -0300

    feat: user proximity_hash

--- lib/features/addressing/data/GeohashServiceAdapter.dart
+++ lib/features/addressing/data/GeohashServiceAdapter.dart
@@ -1,6 +1,13 @@
+import '../../../domain/Location.dart';
+
 abstract class GeohashServiceAdapter {
-  String encode(double lat, double lon, {int precision});
+  String encode(Location location, {int precision});
   List<double> decode(String geohash);
   String findCommonGeohash(
       {String topLeft, String topRight, String bottomLeft, String bottomRight});
+
+  List<String> createGeohashes(Location location, double radius, int precision);
+  bool inCircleCheck(Location location1, Location location2, double radius);
+
+  List<String> proximityGeohashes(Location location, double radius);
 }
--- lib/features/addressing/data/GeohashServiceAdapterImpl.dart
+++ lib/features/addressing/data/GeohashServiceAdapterImpl.dart
@@ -1,5 +1,7 @@
 import 'package:dart_geohash/dart_geohash.dart';
+import 'package:grocery_brasil_app/domain/Location.dart';
 import 'package:meta/meta.dart';
+import 'package:proximity_hash/proximity_hash.dart' as proximity_hash;
 
 import 'GeohashServiceAdapter.dart';
 
@@ -14,8 +16,8 @@ class GeohashServiceAdapterImpl implements GeohashServiceAdapter {
   }
 
   @override
-  String encode(double lat, double lon, {int precision: 9}) {
-    return geoHasher.encode(lon, lat, precision: precision);
+  String encode(Location location, {int precision: 9}) {
+    return geoHasher.encode(location.lon, location.lat, precision: precision);
   }
 
   @override
@@ -39,5 +41,39 @@ class GeohashServiceAdapterImpl implements GeohashServiceAdapter {
     } while (true);
 
     return topLeft.substring(0, i);
+  }
+
+  @override
+  List<String> createGeohashes(
+      Location location, double radius, int precision) {
+    return proximity_hash.createGeohashes(
+        location.lat, location.lon, radius, precision);
+  }
+
+  @override
+  bool inCircleCheck(Location location1, Location location2, double radius) {
+    return proximity_hash.inCircleCheck(
+        location1.lat, location1.lon, location2.lat, location2.lon, radius);
+  }
+
+  @override
+  List<String> proximityGeohashes(Location location, double radius) {
+    // Geohash precision
+    //     #   km
+    // 1   ± 2500
+    // 2   ± 630
+    // 3   ± 78
+    // 4   ± 20
+    // 5   ± 2.4
+    // 6   ± 0.61
+    // 7   ± 0.076
+
+    int precision = 0;
+    List<String> geohashesList;
+    do {
+      precision++;
+      geohashesList = createGeohashes(location, radius, precision);
+    } while (geohashesList.length < 10);
+    return createGeohashes(location, radius, precision - 1);
   }
 }
