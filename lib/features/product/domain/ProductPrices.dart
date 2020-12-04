c736a066c5d04da01fdb3af5fffaa64903c30fc2
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2020-12-04 14:31:44 -0300

    feat: ProductPrices.fromJson

--- lib/features/product/domain/ProductPrices.dart
+++ lib/features/product/domain/ProductPrices.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 import '../../../domain/Company.dart';
 import '../../../domain/Product.dart';
+part 'ProductPrices.g.dart';
 
+@JsonSerializable(explicitToJson: true)
 class ProductPrices extends Equatable {
   final Product product;
   final Company company;
@@ -11,6 +16,13 @@ class ProductPrices extends Equatable {
 
   ProductPrices(
       {this.product, this.company, this.unityValue, this.purchaseDate});
+
+  factory ProductPrices.fromJson(Map<String, dynamic> json) =>
+      _$ProductPricesFromJson(json);
+  Map<String, dynamic> toJson() => _$ProductPricesToJson(this);
+
+  @override
+  String toString() => jsonEncode(this.toJson());
 
   @override
   List<Object> get props => [product, company, unityValue, purchaseDate];
