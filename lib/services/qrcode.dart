f899faa4bab61e4881561e1c5e568137c1e5a3df
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-03-15 12:13:25 -0300

    fix: unused file

--- lib/services/qrcode.dart
+++
@@ -1,8 +0,0 @@
-import 'package:barcode_scan/barcode_scan.dart';
-
-Future<String> scanQrcode() async {
-  final options = ScanOptions(restrictFormat: [BarcodeFormat.qr]);
-  ScanResult scanResult = await BarcodeScanner.scan(options: options);
-
-  return scanResult.rawContent;
-}
