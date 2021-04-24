67868029142e6273d2a77242ef5931676b5a496b
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-24 17:15:33 -0300

    feat: add admob id

--- lib/features/admob/widgets/AdHelper.dart
+++ lib/features/admob/widgets/AdHelper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-6350395903807209/4463414109';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-6350395903807209/4060013752';
     }
     throw new UnsupportedError("Unsupported platform");
   }
