8f230620b70372dfdb39b5de7201332b30449bb3
Luis Marcilio Braga de Oliveira <luismarcilio@yahoo.com.br>
2021-04-26 09:01:47 -0300

    feat: add firebase_analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 
 import 'features/initialize_firebase/presentation/pages/initialize_firebase.dart';
@@ -12,6 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Seu supermercado mais barato', home: InitialiseFirebase());
+      title: 'Seu supermercado mais barato',
+      home: InitialiseFirebase(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())
+      ],
+    );
   }
 }
