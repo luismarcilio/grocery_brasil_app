b84f8350a800ad26b373db8d8019ca86c4d5466b
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-04 15:06:21 -0300

    feat: search prices from purchase

--- lib/features/purchase/presentation/pages/FullPurchaseList.dart
+++ lib/features/purchase/presentation/pages/FullPurchaseList.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../domain/Product.dart';
 import '../../../../domain/Purchase.dart';
+import '../../../../domain/PurchaseItem.dart';
 import '../../../../injection_container.dart';
 import '../../../../screens/common/loading.dart';
 import '../../../admob/domain/AddFactory.dart';
 import '../../../admob/domain/DecorateListWithAds.dart';
 import '../../../admob/widgets/BannerInline.dart';
+import '../../../product/domain/ProductSearchModel.dart';
+import '../../../product/presentation/pages/product_prices_screen.dart';
 import '../bloc/purchase_bloc.dart';
 import '../widgets/NFScreensWidgets.dart';
 
@@ -82,10 +86,71 @@ class BuildPurchaseScreen extends StatelessWidget {
         children: _adDecorator.decorate(
             _purchase.purchaseItemList
                 .map<Widget>((purchaseItem) => nFScreensWidgets.newnfItemCard(
-                    onLongPress: null, onTap: null, purchaseItem: purchaseItem))
+                    onLongPress: null,
+                    onTap: () {
+                      _loadProductPricesScreen(
+                        context: context,
+                        purchaseItem: purchaseItem,
+                      );
+                    },
+                    purchaseItem: purchaseItem))
                 .toList(),
             _addFactory,
             frequency),
+      ),
+    );
+  }
+
+  _loadProductPricesScreen(
+      {@required BuildContext context, @required PurchaseItem purchaseItem}) {
+    final ProductSearchModel productSearch = ProductSearchModel(
+        productId: _getProductId(purchaseItem.product),
+        name: purchaseItem.product.name,
+        eanCode: purchaseItem.product.eanCode,
+        ncmCode: purchaseItem.product.ncmCode,
+        normalized: purchaseItem.product.normalized,
+        thumbnail: purchaseItem.product.thumbnail,
+        unity: purchaseItem.product.unity);
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ProductPricesScreen(
+          product: productSearch,
+        ),
+      ),
+    );
+  }
+
+  _getProductId(Product product) {
+// FIXME: Duplicated code with back end
+
+    // getDocId(product: Product): string {
+    //   const normalizedItemName = product.name.replace(/[^a-zA-Z0-9]+/g, "-");
+    //   const productDocId = !product.eanCode
+    //     ? product.ncmCode + "-" + normalizedItemName
+    //     : product.eanCode;
+    //   return productDocId;
+    // }
+
+    final normalizedItemName =
+        product.name.replaceAllMapped(RegExp(r'[^a-zA-Z0-9]+'), (match) => '-');
+    final productDocId = product.eanCode == null || product.eanCode.length == 0
+        ? product.ncmCode + '-' + normalizedItemName
+        : product.eanCode;
+    return productDocId;
+  }
+}
+
+class ProductPricesScreenTest extends StatelessWidget {
+  final Product product;
+  final ProductSearchModel productSearch;
+
+  const ProductPricesScreenTest({Key key, this.product, this.productSearch})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Expanded(
+        child: Text('ProductPricesScreenTest'),
       ),
     );
   }
