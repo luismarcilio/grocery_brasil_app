e11ef90e65d14edd60438c21e3e910eea025f230
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-06 11:11:23 -0300

    fix: initialize mobile ads

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'features/initialize_firebase/presentation/pages/initialize_firebase.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
   di.init();
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
   runApp(MyApp());
 }
 
