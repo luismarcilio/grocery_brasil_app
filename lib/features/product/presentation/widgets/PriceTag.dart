59273b25b7e4f147ca6ee419d929210d413a59fd
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-20 11:07:16 -0300

    feat: disconto

---
+++ lib/features/product/presentation/widgets/PriceTag.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+
+Column priceTag({@required double amount, @required double discount}) {
+  final currencyNumberFormat = NumberFormat("###.00", "pt_BR");
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      new Text(
+        "R\$ ${currencyNumberFormat.format(amount)}",
+        style: TextStyle(color: Colors.indigo[900], fontSize: 12),
+      ),
+      if (discount != null && discount != 0)
+        Text(
+          "Disc: R\$ ${currencyNumberFormat.format(discount)}",
+          style: TextStyle(color: Colors.red[900], fontSize: 12),
+        ),
+    ],
+  );
+}
