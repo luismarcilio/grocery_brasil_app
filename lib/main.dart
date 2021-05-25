37c671140dab02a65cbe98af3c2d7c2b02e4c5df
Luis Marcilio Braga <lbraga@pavlovmedia.com>
2021-05-25 10:27:56 -0300

    feat: remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Seu supermercado mais barato',
       home: InitialiseFirebase(),
       navigatorObservers: [
